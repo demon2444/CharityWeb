b8bd9a334e08478cc09af3cbc7ad903891c2e486
Marcin Osuchowski <demon2444@gmail.com>
2019-10-03 17:12:20 +0200

    Charity Web create Entity

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,36 @@
+package pl.coderslab.charity.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
