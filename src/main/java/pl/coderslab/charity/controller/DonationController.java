1bc9a47febd8bfb8e3a9a44ae0fe0068d9baf7a2
Marcin Osuchowski <demon2444@gmail.com>
2019-10-09 00:13:33 +0200

    Create DonationController

---
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -0,0 +1,27 @@
+package pl.coderslab.charity.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.service.CategoryService;
+import pl.coderslab.charity.service.DonationService;
+import pl.coderslab.charity.service.InstitutionService;
+
+@Controller
+@RequestMapping("/form")
+public class DonationController {
+
+    private CategoryService categoryService;
+    private DonationService donationService;
+    private InstitutionService institutionService;
+
+    @Autowired
+    public DonationController(CategoryService categoryService, DonationService donationService, InstitutionService institutionService) {
+        this.categoryService = categoryService;
+        this.donationService = donationService;
+        this.institutionService = institutionService;
+    }
+
+
+
+}
