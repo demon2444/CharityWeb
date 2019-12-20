a1a514756b132803a179684c9ef2fb1c1b6abe8f
Marcin Osuchowski <demon2444@gmail.com>
2019-12-20 16:55:46 +0100

    working with Cascade types

--- src/main/java/pl/coderslab/charity/model/User.java
+++ src/main/java/pl/coderslab/charity/model/User.java
@@ -38,7 +38,7 @@ public class User {
 
 
 
-    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH}, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH}, fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
